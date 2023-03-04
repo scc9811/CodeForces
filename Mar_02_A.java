46b8080eb1566f800a69ba9ef22cc2aa6b45acba
k s jeon <wjsrudtjr98@naver.com>
2023-03-04 18:44:32 +0900

    edited_A

--- Mar_02_A.java
+++ Mar_02_A.java
@@ -10,61 +10,25 @@ public class Mar_02_A{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        mainLoop:
         while(testCase-->0){
             int n = Integer.parseInt(br.readLine());
-            String s = br.readLine();
-            boolean case1 = true;
-            boolean case2 = false;
-            boolean case3 = false;
-            boolean case4 = false;
-            boolean res = true;
-            for(int i=0; i<n; i++){
-                if(case1){
-                    if(s.charAt(i)=='m'|| s.charAt(i)=='M') continue;
-                    else if(s.charAt(i)=='e' || s.charAt(i)=='E'){
-                        case1 = false;
-                        case2 = true;
-                        continue;
-                    }
-                    else{
-                        res = false;
-                        break;
-                    }
-                }
-                else if(case2){
-                    if(s.charAt(i)=='e'|| s.charAt(i)=='E') continue;
-                    else if(s.charAt(i)=='o' || s.charAt(i)=='O'){
-                        case2 = false;
-                        case3 = true;
-                        continue;
-                    }
-                    else{
-                        res = false;
-                        break;
-                    }
-                }
-                else if(case3){
-                    if(s.charAt(i)=='o'|| s.charAt(i)=='O') continue;
-                    else if(s.charAt(i)=='w' || s.charAt(i)=='W'){
-                        case3 = false;
-                        case4 = true;
-                        continue;
-                    }
-                    else{
-                        res = false;
-                        break;
-                    }
-                }
-                else if(case4){
-                    if(s.charAt(i)=='w' || s.charAt(i)=='W') continue;
-                    else{
-                        res = false;
-                        break;
-                    }
+            String s = br.readLine().toLowerCase();
+            StringBuilder res = new StringBuilder();
+            if(s.charAt(0)=='m'){
+                res.append('m');
+            }
+            else{
+                sb.append("NO").append('\n');
+                continue mainLoop;
+            }
+            for(int i=1; i<n; i++){
+                if(s.charAt(i)!=s.charAt(i-1)){
+                    res.append(s.charAt(i));
                 }
             }
 
-            if(res && case4){
+            if(res.toString().equals("meow")){
                 sb.append("YES").append('\n');
             }
             else{
